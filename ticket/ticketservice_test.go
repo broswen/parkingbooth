47dfb7c4f5e41d39b0169bcb1f6af993e6577894
Brad <broswen@gmail.com>
2021-11-09 20:01:39 -0600

    create ticket service tests

---
+++ ticket/ticketservice_test.go
@@ -0,0 +1,84 @@
+package ticket
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/broswen/parkingbooth/repository"
+)
+
+func TestGenerateTicket(t *testing.T) {
+	repo, err := repository.NewRepository("map")
+	if err != nil {
+		t.Fatalf("init map repo: %v\n", err)
+	}
+
+	service, err := NewService(repo)
+	if err != nil {
+		t.Fatalf("init ticket service: %v\n", err)
+	}
+
+	t1, err := service.GenerateTicket("test")
+	if err != nil {
+		t.Fatalf("generate ticket: %v\n", err)
+	}
+
+	t2, err := service.GetTicket(t1.Location, t1.Id)
+	if err != nil {
+		t.Fatalf("get ticket: %v\n", err)
+	}
+
+	if !reflect.DeepEqual(t1, t2) {
+		t.Fatalf("tickets don't match")
+	}
+
+}
+
+func TestCompleteTicket(t *testing.T) {
+	repo, err := repository.NewRepository("map")
+	if err != nil {
+		t.Fatalf("init map repo: %v\n", err)
+	}
+
+	service, err := NewService(repo)
+	if err != nil {
+		t.Fatalf("init ticket service: %v\n", err)
+	}
+
+	t1, err := service.GenerateTicket("test")
+	if err != nil {
+		t.Fatalf("generate ticket: %v\n", err)
+	}
+
+	t1, err = service.CompleteTicket(t1.Location, t1.Id)
+	if err != nil {
+		t.Fatalf("complete ticket: %v\n", err)
+	}
+}
+
+func TestPayTicket(t *testing.T) {
+	repo, err := repository.NewRepository("map")
+	if err != nil {
+		t.Fatalf("init map repo: %v\n", err)
+	}
+
+	service, err := NewService(repo)
+	if err != nil {
+		t.Fatalf("init ticket service: %v\n", err)
+	}
+
+	t1, err := service.GenerateTicket("test")
+	if err != nil {
+		t.Fatalf("generate ticket: %v\n", err)
+	}
+
+	t1, err = service.CompleteTicket(t1.Location, t1.Id)
+	if err != nil {
+		t.Fatalf("complete ticket: %v\n", err)
+	}
+
+	t1, err = service.PayTicket(t1.Location, t1.Id, "test")
+	if err != nil {
+		t.Fatalf("pay ticket: %v\n", err)
+	}
+}
